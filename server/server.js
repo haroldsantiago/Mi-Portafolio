7df634ca461b51df850bf0d3a4709dbd5626625b
Harold <santitover1@gmail.com>
2025-12-02 19:14:37 -0500

    ejuste

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import {supabase} from "./db.js";
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/reviews", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("reviews").select("*");
+    if (error) throw error;
+    res.json(data);
+  } catch (err) {
+    console.error("🔥 Error en GET /api/reviews:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST: crear review
+app.post("/api/reviews", async (req, res) => {
+  const { name, comment, rating } = req.body;
+  const { data, error } = await supabase
+    .from("reviews")
+    .insert([{ name, comment, rating }])
+    .select();
+  if (error) return res.status(500).json({ error: error.message });
+  res.status(201).json(data[0]);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ Servidor en https://restaurante-3hur.onrender.com/${PORT}`));
